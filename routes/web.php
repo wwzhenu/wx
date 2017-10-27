d34b91e238146e94826da88fc6a44f780495cfd0
wangwenzeng <272346623@qq.com>
2017-10-27 17:23:13 +0800

    用以微信服务器验证

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Http\Request;
+Route::get('/', function (Request $request) {
+    $parm=array(
+        'wanglovechu',
+        $request->get('timestamp'),
+        $request->get('timestamp')
+    );
+    sort($parm,SORT_STRING);
+    $parm=sha1(implode('',$parm));
+    if ($parm==$request->get('signature')){
+        echo $request->get('echostr');
+    }else{
+        echo '非微信访问';
+    }
 });
