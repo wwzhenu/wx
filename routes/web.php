41793a661ae79b7d543c00b86e59524fb53fae37
wangwenzeng <272346623@qq.com>
2017-10-27 17:45:09 +0800

    调试

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Route::get('/', function (Request $request) {
         $request->get('timestamp'),
         $request->get('timestamp')
     );
+    file_put_contents('1.txt',print_r($parm,1));
     sort($parm,SORT_STRING);
     $parm=sha1(implode($parm));
+    file_put_contents('2.txt',print_r($parm,1));
+    file_put_contents('3.txt',print_r($request->get('signature'),1));
     if ($parm==$request->get('signature')){
         echo $request->get('echostr');
     }else{
